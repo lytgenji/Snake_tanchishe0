f572d5af36c0bc6a2364eb591009af2db5754c89
genjilee <957867991@qq.com>
2017-05-11 13:20:54 +0800

    Signed-off-by: genjilee <957867991@qq.com>

---
+++ Snake2/app/src/main/java/com/example/android/snake/Snake.java
@@ -0,0 +1,58 @@
+
+
+package com.example.android.snake;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
+
+
+public class Snake extends Activity {
+
+    private SnakeView mSnakeView;
+    
+    private static String ICICLE_KEY = "snake-view";
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // No Title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.snake_layout);
+
+        mSnakeView = (SnakeView) findViewById(R.id.snake);
+        mSnakeView.setTextView((TextView) findViewById(R.id.text));
+
+// 检查存贮状态以确定是重新开始还是恢复状态
+        if (savedInstanceState == null) {
+            // We were just launched -- set up a new game
+            mSnakeView.setMode(SnakeView.READY);
+        } else {
+            // We are being restored
+            Bundle map = savedInstanceState.getBundle(ICICLE_KEY);
+            if (map != null) {
+                mSnakeView.restoreState(map);
+            } else {
+                mSnakeView.setMode(SnakeView.PAUSE);
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Pause the game along with the activity
+        mSnakeView.setMode(SnakeView.PAUSE);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        //Store the game state
+        outState.putBundle(ICICLE_KEY, mSnakeView.saveState());
+    }
+
+}
